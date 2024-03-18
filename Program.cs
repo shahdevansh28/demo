68fc1d5f66f179365ed3335dd8afa15f9ab51eb8
Devansh Shah <86820517+shahdevansh28@users.noreply.github.com>
2024-03-18 12:15:05 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,7 +3,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.WebHost.UseUrls($"http://*:80");
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8081";
+builder.WebHost.UseUrls($"http://*:{port}");
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
